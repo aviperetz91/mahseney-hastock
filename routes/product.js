7a818135dd1bda76c18f439cf20b4bb5b13ace97
unknown <a.peretz33@gmail.com>
2019-11-24 15:05:40 +0200

    Products by sell and arrival, related products

--- routes/product.js
+++ routes/product.js
@@ -27,7 +27,6 @@ router.get('/product/:productId', (req, res) => {
 // @desc    Create product
 // @access  Private
 router.post('/product/create/:userId', requireLogin, isAuth, isAdmin, (req, res) => {
-
     let form = new formidable.IncomingForm();
     form.keepExtensions = true;
     form.parse(req, (err, fields, files) => {
@@ -86,7 +85,7 @@ router.put('/product/:productId/:userId', requireLogin, isAuth, isAdmin, (req, r
 
         let product = req.product;
 
-        // Updating the product
+        // Replacing product fields with the new fields
         product = _.extend(product, fields);
 
         // Check if there is a photo
@@ -121,6 +120,56 @@ router.delete('/product/:productId/:userId', requireLogin, isAuth, isAdmin, (req
         .catch(err => console.log(err));
 });
 
+
+// ********** Sell / Arrival **********
+// by sell = /products?sortBy=sold&order=desc&limit=4
+// by arrival = /products?sortBy=createdAt&order=desc&limit=4
+// if no params are sent, then all products are returned
+
+// @route   GET api/products
+// @desc    Get list of products by sell / by arrival
+// @access  Public
+router.get('/products', (req, res) => {
+    let order = req.query.order ? req.query.order : 'asc';
+    let sortBy = req.query.sortBy ? req.query.sortBy : '_id';
+    let limit = req.query.limit ? +req.query.limit : 6;
+
+    Product.find()
+        .select('-photo')
+        .populate('category')
+        .sort([[sortBy, order]])
+        .limit(limit)
+        .exec((err, products) => {
+            if(err) {
+                return res.status(400).json({ error: 'Products not found' });
+            }
+            res.json(products);
+        })
+})
+
+
+// ********** Related Products **********
+// it will find the products based on the req product category,
+// other products that has the same category will be returned
+
+// @route   GET api/products/related/:productId
+// @desc    Get list of related products
+// @access  Public
+router.get('/products/related/:productId', (req, res) => {
+    let limit = req.query.limit ? + req.query.limit : 6;
+
+    // Not include the current product
+    Product.find({ _id: { $ne: req.product }, category: req.product.category })
+    .limit(limit)
+    .populate('category', '_id title')
+    .exec((err, products) => {
+        if(err) {
+            return res.status(400).json({ error: 'Products not found' });
+        }
+        // console.log(products);
+        res.json(products);
+    })
+})
 
 router.param('productId', productById);
 router.param('userId', userById);
